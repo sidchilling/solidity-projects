8a09a8c9f22aaf766ccae4f6aa208786720a93a8
Sid Saha <ssaha@nerdwallet.com>
2018-04-03 19:34:52 +0530

    adding test for `getKitty` function

--- cryptozombies/test/testCryptoKitty.js
+++ cryptozombies/test/testCryptoKitty.js
@@ -51,3 +51,54 @@ contract('CryptoKitty', () => {
     });
 
 });
+
+contract('GetKittyTest', () => {
+
+    it('create and get kitty', () => {
+        let kittyInstance = null;
+        const stringWithIdSet = 200;
+        const nameString = 'Another Sid Kitty';
+        let createdKittyId;
+        return CryptoKitty.deployed()
+        .then(instance => {
+            kittyInstance = instance;
+            eventWatcher = new EventWatcher(kittyInstance, 'KittyCreated');
+            return kittyInstance.addKitty(stringWithIdSet, nameString);
+        })
+        .then(() => eventWatcher.run())
+        .then(() => {
+            return eventWatcher.getAllArgs()
+            .then(allArgs => {
+                const currentArgs = allArgs[0];
+                createdKittyId = currentArgs.kittyId;
+                console.log('Created Kitty ID: ' + createdKittyId);
+            });
+        })
+        .then(() => {
+            return kittyInstance.getKitty(createdKittyId)
+            .then(data => {
+                const isGestating = data[0];
+                const isReady = data[1];
+                const cooldownIndex = parseInt(data[2]);
+                const nextActionAt = parseInt(data[3]);
+                const stringWithId = parseInt(data[4]);
+                const birthTime = parseInt(data[5]);
+                const matronId = parseInt(data[6]);
+                const sireId = parseInt(data[7]);
+                const generation = parseInt(data[8]);
+                const genes = parseInt(data[9]);
+                assert.isFalse(isGestating, 'isGestating must be false');
+                assert.isTrue(isReady, 'isReady must be true');
+                assert.equal(cooldownIndex, 0, 'cooldownIndex must be 0');
+                assert.equal(nextActionAt, 0, 'nextActionAt must be 0');
+                assert.equal(birthTime, 0, 'birthTime must be 0');
+                assert.equal(matronId, 0, 'matronId must be 0');
+                assert.equal(sireId, 0, 'sireId must be 0');
+                assert.equal(generation, 0, 'generation must be 0');
+                assert.equal(stringWithId, stringWithIdSet, 'ID strings must be equal');
+                console.log('Gene: ' + genes);
+            });
+        });
+    });
+
+});
